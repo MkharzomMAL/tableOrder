8ab63877de1a342b61851a01ca1704f8e9b12470
MkharzomMAL <kharzom.mohammed.mal@gmail.com>
2023-05-29 11:20:17 +0800

    table model

---
+++ database/migrations/2023_05_29_031128_create_tables_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tables', function (Blueprint $table) {
+            $table->id();
+            $table->text('code');
+            $table->string('statue')->default('available') ;
+            $table->boolean('enable')->default(true);
+            $table->boolean('printed')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tables');
+    }
+}
